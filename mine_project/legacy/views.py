139f9d37d596960a3f5db81c72a23aec136e7408
Nick Morales <nmorales3142@gmail.com>
2014-09-02 15:38:08 -0400

    test

--- mine_project/legacy/views.py
+++ mine_project/legacy/views.py
@@ -224,3 +224,5 @@ def select_legacy_row(request, legacy_row, legacy_seed):
   context_dict['exp_list'] = exp_list
   context_dict['logged_in_user'] = request.user.username
   return render_to_response('legacy/legacy_row.html', context_dict, context)
+
+  """testing"""
