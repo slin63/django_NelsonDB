4b59ceeb6be9cb4f437b5cb2803e5e5ea5db0f32
Surya Saha <ss2489@cornell.edu>
2014-12-19 16:34:30 -0500

    fixed the auto generated blocks for metamaize tables

--- metamaize/models.py
+++ metamaize/models.py
@@ -3,9 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+# Keeping `managed = False` to disallow Django from creating, modifying or deleting tables
 class Citation(models.Model):
+    #type is enum field in DB
+    citation_type_choices = (
+        ('literature','literature'),
+        ('website','website'),
+        ('personal','personal'),
+        ('other','other'),
+    )
     citation_id = models.IntegerField(db_column='Citation_ID', primary_key=True)  # Field name made lowercase.
-    type = models.CharField(db_column='Type', max_length=10)  # Field name made lowercase.
+    citation_type = models.CharField(db_column='Type', max_length=10, choices=citation_type_choices)  # Field name made lowercase.
     url = models.CharField(db_column='URL', max_length=50, blank=True)  # Field name made lowercase.
     pubmed_id = models.CharField(db_column='Pubmed_ID', max_length=10, blank=True)  # Field name made lowercase.
     notes = models.CharField(db_column='Notes', max_length=2000, blank=True)  # Field name made lowercase.
@@ -16,6 +24,19 @@ class Citation(models.Model):
 
 
 class Culture(models.Model):
+    #type is enum field in DB
+    microbe_type_observed_choices = (
+        ('Fungi','Fungi'),
+        ('Bacteria','Bacteria'),
+        ('Both','Fungi and Bacteria'),
+        ('0','0'),
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5'),
+        ('Na','NA'),
+    )
     culture_id = models.IntegerField(db_column='Culture_ID', primary_key=True)  # Field name made lowercase.
     row = models.ForeignKey('Temprow', db_column='Row_ID')  # Field name made lowercase.
     pedigree_label = models.ForeignKey('Temppedigree', db_column='Pedigree_Label')  # Field name made lowercase.
@@ -23,7 +44,7 @@ class Culture(models.Model):
     medium = models.ForeignKey('Medium', db_column='Medium_ID')  # Field name made lowercase.
     tissue = models.ForeignKey('Tissue', db_column='Tissue_ID')  # Field name made lowercase.
     culture_name = models.CharField(db_column='Culture_Name', max_length=30)  # Field name made lowercase.
-    microbe_type_observed = models.CharField(db_column='Microbe_Type_Observed', max_length=8, blank=True)  # Field name made lowercase.
+    microbe_type_observed = models.CharField(db_column='Microbe_Type_Observed', max_length=8, blank=True, choice=microbe_type_observed_choices)  # Field name made lowercase.
     plating_cycle = models.IntegerField(db_column='Plating_Cycle')  # Field name made lowercase.
     dilution = models.CharField(db_column='Dilution', max_length=10)  # Field name made lowercase.
     image_location = models.CharField(db_column='Image_Location', max_length=100, blank=True)  # Field name made lowercase.
@@ -45,10 +66,17 @@ class Medium(models.Model):
 
 
 class Microbe(models.Model):
+    #type is enum field in DB
+    microbe_type_choices = (
+        ('Fungi','Fungi'),
+        ('Bacteria','Bacteria'),
+        ('Archaea','Archaea'),
+        ('Unknown','Unknown'),
+    )
     microbe_id = models.IntegerField(db_column='Microbe_ID', primary_key=True)  # Field name made lowercase.
     source_culture = models.ForeignKey(Culture, db_column='Source_Culture', blank=True, null=True)  # Field name made lowercase.
     source_tissue = models.ForeignKey('Tissue', db_column='Source_Tissue', blank=True, null=True)  # Field name made lowercase.
-    type = models.CharField(db_column='Type', max_length=8, blank=True)  # Field name made lowercase.
+    microbe_type = models.CharField(db_column='Type', max_length=8, blank=True, choices=microbe_type_choices)  # Field name made lowercase.
     sequence = models.ForeignKey('MicrobeSequence', db_column='Sequence_ID', blank=True, null=True)  # Field name made lowercase.
     notes = models.CharField(db_column='Notes', max_length=2000, blank=True)  # Field name made lowercase.
 
@@ -86,11 +114,20 @@ class Person(models.Model):
 
 
 class Primer(models.Model):
+    #direction and barcode are enum fields in DB
+    direction_choices = (
+        ('F','Forward'),
+        ('R','Reverse'),
+    )
+    barcode_choices = (
+        ('Y','Yes'),
+        ('N','No'),
+    )
     primer_id = models.IntegerField(db_column='Primer_ID', primary_key=True)  # Field name made lowercase.
     primer_number = models.CharField(db_column='Primer_Number', max_length=10)  # Field name made lowercase.
     sequence = models.CharField(db_column='Sequence', max_length=255)  # Field name made lowercase.
-    direction = models.CharField(db_column='Direction', max_length=1)  # Field name made lowercase.
-    barcode = models.CharField(db_column='Barcode', max_length=1)  # Field name made lowercase.
+    direction = models.CharField(db_column='Direction', max_length=1, choices=direction_choices)  # Field name made lowercase.
+    barcode = models.CharField(db_column='Barcode', max_length=1, choices=barcode_choices)  # Field name made lowercase.
     target = models.CharField(db_column='Target', max_length=255, blank=True)  # Field name made lowercase.
     purpose = models.CharField(db_column='Purpose', max_length=255, blank=True)  # Field name made lowercase.
     notes = models.CharField(db_column='Notes', max_length=2000, blank=True)  # Field name made lowercase.
@@ -115,8 +152,20 @@ class Source(models.Model):
 
 
 class Tissue(models.Model):
+    #type is enum field in DB
+    tissue_type_choices = (
+        ('planted_kernel','Planted Kernel'),
+        ('leaf','Leaf'),
+        ('root','Root'),
+        ('stem','Stem'),
+        ('prop_root','Adventitious or prop roots'),
+        ('immature_ear','Immature ears'),
+        ('harvested_kernel','Harvested Kernel'),
+        ('other','Other'),
+    )
+
     tissue_id = models.IntegerField(db_column='Tissue_ID', primary_key=True)  # Field name made lowercase.
-    type = models.CharField(db_column='Type', max_length=16)  # Field name made lowercase.
+    tissue_type = models.CharField(db_column='Type', max_length=16, choices=tissue_type_choices)  # Field name made lowercase.
     sample_name = models.CharField(db_column='Sample_Name', max_length=40)  # Field name made lowercase.
     date_ground = models.DateField(db_column='Date_Ground', blank=True, null=True)  # Field name made lowercase.
     date_plated = models.DateField(db_column='Date_Plated', blank=True, null=True)  # Field name made lowercase.
@@ -127,6 +176,25 @@ class Tissue(models.Model):
     class Meta:
         managed = False
         db_table = 'Tissue'
+
+class Temppedigree(models.Model):
+    pedigree_label = models.CharField(db_column='Pedigree_Label', primary_key=True, max_length=8)  # Field name made lowercase.
+    environment = models.CharField(db_column='Environment', max_length=11)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'tempPedigree'
+
+
+class Temprow(models.Model):
+    row_id = models.CharField(db_column='Row_ID', primary_key=True, max_length=10)  # Field name made lowercase.
+    pedigree_label = models.ForeignKey(Temppedigree, db_column='Pedigree_Label')  # Field name made lowercase.
+    source = models.CharField(db_column='Source', max_length=5)  # Field name made lowercase.
+    location = models.CharField(db_column='Location', max_length=2)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'tempRow'
 
 
 class AuthGroup(models.Model):
@@ -243,22 +311,3 @@ class DjangoSession(models.Model):
         managed = False
         db_table = 'django_session'
 
-
-class Temppedigree(models.Model):
-    pedigree_label = models.CharField(db_column='Pedigree_Label', primary_key=True, max_length=8)  # Field name made lowercase.
-    environment = models.CharField(db_column='Environment', max_length=11)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'tempPedigree'
-
-
-class Temprow(models.Model):
-    row_id = models.CharField(db_column='Row_ID', primary_key=True, max_length=10)  # Field name made lowercase.
-    pedigree_label = models.ForeignKey(Temppedigree, db_column='Pedigree_Label')  # Field name made lowercase.
-    source = models.CharField(db_column='Source', max_length=5)  # Field name made lowercase.
-    location = models.CharField(db_column='Location', max_length=2)  # Field name made lowercase.
-
-    class Meta:
-        managed = False
-        db_table = 'tempRow'
