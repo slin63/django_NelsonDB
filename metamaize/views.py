256ad8ba7f37d8320aebf70fe92b67885097f905
Surya Saha <ss2489@cornell.edu>
2015-01-01 13:56:35 -0500

    removed todo line

--- metamaize/views.py
+++ metamaize/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from metamaize.models import Citation, Culture, Medium, Microbe, MicrobeSequence, Person, Primer, Source, Tissue, Temppedigree, Temprow
-from metamaize.forms import TODO
 #UserForm, UserProfileForm, ChangePasswordForm, EditUserForm, EditUserProfileForm, NewExperimentForm
 
 def index(request):
