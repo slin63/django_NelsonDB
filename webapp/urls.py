74b1ae3426bdc072a4a0ec6410c8ea5a61a8ded9
Surya Saha <ss2489@cornell.edu>
2015-01-01 13:17:26 -0500

    added metamaize app url

--- webapp/urls.py
+++ webapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'webapp.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^metamaize/', include('metamaize.urls')),
     url(r'^$', include('lab.urls')),
     url(r'^lab/', include('lab.urls')),
     url(r'^legacy/', include('legacy.urls')),
