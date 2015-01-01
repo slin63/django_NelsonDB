46ed8fe5497e1aaf0be9d95f23dac679fe430470
Surya Saha <ss2489@cornell.edu>
2015-01-01 12:50:10 -0500

    added includes and test index func

--- metamaize/views.py
+++ metamaize/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from metamaize.models import Citation, Culture, Medium, Microbe, MicrobeSequence, Person, Primer, Source, Tissue, Temppedigree, Temprow
+from metamaize.forms import TODO
+#UserForm, UserProfileForm, ChangePasswordForm, EditUserForm, EditUserProfileForm, NewExperimentForm
+
+def index(request):
+	return HttpResponse("Metamaize is alive!!")
