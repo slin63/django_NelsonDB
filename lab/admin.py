a0d93e3924f1091185280323b85d7047664f35fb
Nick Morales <nmorales3142@gmail.com>
2015-02-16 15:59:39 -0500

    Added django admin for upload queue

--- lab/admin.py
+++ lab/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from lab.models import UserProfile, User, Experiment, Locality, Field, Taxonomy, Passport, Location, Stock, StockPacket
+from lab.models import UserProfile, User, Experiment, Locality, Field, Taxonomy, Passport, Location, Stock, StockPacket, UploadQueue
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'last_login', 'date_joined')
@@ -8,7 +8,14 @@ class UserAdmin(admin.ModelAdmin):
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ('user', 'phone', 'organization', 'job_title', 'notes', 'website', 'picture')
 
+class ExperimentAdmin(admin.ModelAdmin):
+	list_display = ('user', 'field', 'name', 'start_date', 'purpose', 'comments')
+
+class UploadQueueAdmin(admin.ModelAdmin):
+	list_display = ('experiment', 'user', 'file_name', 'upload_type', 'date', 'completed', 'comments')
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UploadQueue, UploadQueueAdmin)
+admin.site.register(Experiment, ExperimentAdmin)
--- legacy/admin.py
+++ legacy/admin.py
@@ -1,11 +1,1 @@
 from django.contrib import admin
-from legacy.models import Legacy_Seed, Legacy_Row
-
-class Legacy_SeedAdmin(admin.ModelAdmin):
-  list_display = ('seed_id', 'seed_name')
-
-class Legacy_RowAdmin(admin.ModelAdmin):
-  list_display = ('row_id', 'pedigree')
-
-admin.site.register(Legacy_Seed, Legacy_SeedAdmin)
-admin.site.register(Legacy_Row, Legacy_RowAdmin)
