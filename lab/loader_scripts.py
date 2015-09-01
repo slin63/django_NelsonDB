494138c5bf47c9f322e3fbc6e68bf1b12f30ba4b
Nick Morales <nmorales3142@gmail.com>
2015-09-01 10:32:28 -0400

    troubleshoot loader

--- lab/loader_scripts.py
+++ lab/loader_scripts.py
@@ -274,11 +274,12 @@ def seed_stock_loader_prep(upload_file, user):
                 obs_tracker_source_new[(obs_tracker_source_id, obs_tracker_hash_table[obs_tracker_exp_hash], temp_targetobs_id)] = obs_tracker_source_id
                 obs_tracker_source_id = obs_tracker_source_id + 1
 
-            obs_tracker_source_row_hash = str(obs_tracker_obs_row_id_table[obs_row_id][0]) + str(temp_targetobs_id)
-            if obs_tracker_source_row_hash not in obs_tracker_source_hash_table and obs_row_id != 1:
-                obs_tracker_source_hash_table[obs_tracker_source_row_hash] = obs_tracker_source_id
-                obs_tracker_source_new[(obs_tracker_source_id, obs_tracker_obs_row_id_table[obs_row_id][0], temp_targetobs_id)] = obs_tracker_source_id
-                obs_tracker_source_id = obs_tracker_source_id + 1
+            if obs_row_id != 1:
+                obs_tracker_source_row_hash = str(obs_tracker_obs_row_id_table[obs_row_id][0]) + str(temp_targetobs_id)
+                if obs_tracker_source_row_hash not in obs_tracker_source_hash_table:
+                    obs_tracker_source_hash_table[obs_tracker_source_row_hash] = obs_tracker_source_id
+                    obs_tracker_source_new[(obs_tracker_source_id, obs_tracker_obs_row_id_table[obs_row_id][0], temp_targetobs_id)] = obs_tracker_source_id
+                    obs_tracker_source_id = obs_tracker_source_id + 1
 
     end = time.clock()
     stats = {}
