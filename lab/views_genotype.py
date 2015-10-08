39e85277696b31981baa13fc661bf4cd5ac6b2c5
Nick Morales <nmorales3142@gmail.com>
2015-10-08 12:03:23 -0400

    browse genotype data with d3js plot

--- lab/views_genotype.py
+++ lab/views_genotype.py
@@ -32,17 +32,22 @@ def genotype_data_browse(request):
 def genotype_data_browse_plot(request):
     context = RequestContext(request)
     context_dict = {}
-    all_qtl = QTL.objects.all()
-    all_markers = Marker.objects.all()
+    all_qtl = QTL.objects.all().exclude(id=1)
+    all_markers = Marker.objects.all().exclude(id=1)
+    all_expression = MapFeatureExpression.objects.all()
 
-    qtl_list = []
-    markers_list = []
-    expression_list = []
+    data = []
 
     for q in all_qtl:
-        qtl_list.append([q.map_feature_interval.map_feature_start.physical_position, q.map_feature_interval.map_feature_start.chromosome, '%s Start'%(q.parameter)])
-        qtl_list.append([q.map_feature_interval.map_feature_end.physical_position, q.map_feature_interval.map_feature_end.chromosome, '%s End'%(q.parameter)])
+        data.append({'position':q.map_feature_interval.map_feature_start.physical_position, 'chromosome':q.map_feature_interval.map_feature_start.chromosome, 'name':'%s Start'%(q.parameter), 'type':'QTL'})
+        data.append({'position':q.map_feature_interval.map_feature_end.physical_position, 'chromosome':q.map_feature_interval.map_feature_end.chromosome, 'name':'%s End'%(q.parameter), 'type':'QTL'})
     for m in all_markers:
-        markers_list.append([m.marker_map_feature.physical_position, m.marker_map_feature.chromosome, m.marker_id])
+        data.append({'position':m.marker_map_feature.physical_position, 'chromosome':m.marker_map_feature.chromosome, 'name':m.marker_id, 'type':'Marker'})
+        #data.append({'position':m.map_feature_interval.map_feature_start.physical_position, 'chromosome':m.map_feature_interval.map_feature_start.chromosome, 'name':'%s Start'%(m.marker_id), 'type':'MarkerR'})
+        #data.append({'position':m.map_feature_interval.map_feature_end.physical_position, 'chromosome':m.map_feature_interval.map_feature_end.chromosome, 'name':'%s End'%(m.marker_id), 'type':'MarkerR'})
+    for e in all_expression:
+        data.append({'position':e.map_feature_interval.map_feature_start.physical_position, 'chromosome':e.map_feature_interval.map_feature_start.chromosome, 'name':'%s Start Value: %s'%(e.parameter, e.value), 'type':'Expression'})
+        data.append({'position':e.map_feature_interval.map_feature_end.physical_position, 'chromosome':e.map_feature_interval.map_feature_end.chromosome, 'name':'%s End Value: %s'%(e.parameter, e.value), 'type':'Expression'})
+    print(data)
 
-    return JsonResponse({'qtl':qtl_list, 'markers':markers_list, 'expression':expression_list}, safe=True)
+    return JsonResponse({'data':data}, safe=True)
--- static/js/genotype.js
+++ static/js/genotype.js
@@ -6,37 +6,100 @@ $(document).ready(function(){
     url: "/lab/data/genotype/browse/plot/",
     success: function(data) {
       console.log(data);
-      $('#overview_chart').jqplot([data.qtl, data.markers, data.expression],{
-        seriesDefaults:{
-          renderer:$.jqplot.BlockRenderer
-        },
-        legend:{
-          renderer: $.jqplot.EnhancedLegendRenderer,
-          show:true
-        },
-        series: [
-        {},
-        {rendererOptions: {
-          css:{background:'#A1EED6'}
-        }},
-        {rendererOptions: {
-          css:{background:'#D3E4A0'}
-        }}
-        ],
-        axes: {
-          //bp positions on chromosome
-          xaxis: {
-            min:0,
-            max: 300000000
-          },
-          //chromosomes
-          yaxis: {
-            min: 1,
-            max: 10
-          }
-        }
+      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 1280 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
+
+      var x = d3.scale.linear()
+      .range([0, width]);
+
+      var y = d3.scale.linear()
+      .range([height, 0]);
+
+      var color = d3.scale.category10();
+
+      var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+      var svg = d3.select("#overview_chart").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      data.data.forEach(function(d) {
+        d.chromosome = +d.chromosome;
+        d.position = +d.position;
       });
+
+      x.domain(d3.extent(data.data, function(d) { return d.position; })).nice();
+      y.domain(d3.extent(data.data, function(d) { return d.chromosome; })).nice();
+
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("Position (bp)");
+
+      svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Chromosome")
+
+      svg.selectAll(".dot")
+      .data(data.data)
+      .enter().append("circle")
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.position); })
+      .attr("cy", function(d) { return y(d.chromosome); })
+      .style("fill", function(d) { return color(d.type); })
+      .on("mouseover", function(d) {
+        d3.select(this).style("fill", "white");
+        $(this).popover({html:true, title:d.name, placement:'right', container:'body', content: 'Chromosome: '+d.chromosome+'<br/>Type: '+d.type+'<br/>Position (bp): '+d.position}).popover('show');
+      })
+      .on("mouseout",  function(d) {
+        d3.select(this).style("fill", color(d.type));
+        $(this).popover("hide");
+      });
+
+      var legend = svg.selectAll(".legend")
+      .data(color.domain())
+      .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+      legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+      legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
     }
   });
+
 
 });
