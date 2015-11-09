5223cdbed9d3e14a17d7b6788ca0f08e6a8f270b
Nick Morales <nmorales3142@gmail.com>
2015-11-09 09:47:00 -0500

    minor change to exp form

--- lab/forms.py
+++ lab/forms.py
@@ -72,7 +72,7 @@ class EditUserProfileForm(forms.ModelForm):
 class NewExperimentForm(forms.Form):
 	user = forms.ModelChoiceField(queryset=User.objects.all(), empty_label="--- Username ---", help_text="Select the primary user:", required=True)
 	field = forms.ModelChoiceField(queryset=Field.objects.all(), empty_label="--- Field Name ---", help_text="Select a field or select 'No Field':", required=True)
-	name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Experiment Name'}), help_text="Assign an experiment name (e.g. 15XY with NO SPACES or SPECIAL CHARACTERS):", required=True)
+	name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Experiment Name'}), help_text="Assign an experiment name (e.g. 15XY with NO SPACES, UNDERLINES, or SPECIAL CHARACTERS):", required=True)
 	start_date = forms.DateField(widget=forms.DateInput(attrs={'placeholder':'Start Date'}), help_text="Give a start date in format MM/DD/YYYY:", required=True)
 	purpose = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Purpose'}), help_text="Description of purpose:", required=True)
 	comments = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Comments', 'rows': '5', 'cols': '20'}), help_text="Any additional comments:", required=False)
