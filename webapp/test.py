b5882a26e447f1f2ae6ef7ba423efb8bf3d5d172
root <root@localhost.localdomain>
2016-05-23 12:05:58 -0500

    fixed untracked files

---
+++ webapp/test.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# root_path should return /path/to/site_directory
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Should lead to site-packages
+sys.path.insert(0, os.path.abspath(os.path.join(root_path, 'env/lib/python2.7/site-packages/')))
+print 'sys.path', sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(root_path)))
+sys.path.insert(0, os.path.abspath(os.path.join(root_path, 'webapp')))
+print 'sys.path', sys.path
+os.environ['DJANGO_SETTINGS_MODULE'] = 'webapp.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
