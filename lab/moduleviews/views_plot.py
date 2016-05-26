f9c395e898fc34a10a981411c740a313315659c7
slin63 <slin63@illinois.edu>
2016-05-26 15:44:48 -0500

    Slight changes to spreadsheet rendering

--- lab/moduleviews/views_plot.py
+++ lab/moduleviews/views_plot.py
@@ -119,13 +119,12 @@ def download_field_map(request):
     plot_id = obs.obs_plot.plot_id
     row_num = obs.obs_plot.row_num
     range_num = field_map_generator.number_to_letter(obs.obs_plot.range_num)
-    exp = obs.experiment.name + ': ' + obs.experiment.start_date
 
     coords = range_num + str(row_num)
     plot_dict[coords] = plot_id
     rows.append(int(row_num))
     ranges.append(range_num)
-    experiments.append(exp)
+    experiments.append(obs.experiment)
 
   domain = [rows, ranges]
   info = (plot_dict, domain, set(experiments))
